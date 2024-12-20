a28164d7d5f2b150603d59a3b3e926195d9714e1
Prime <wolabash@gmail.com>
2024-12-20 12:12:09 +0000

    Feat: resolving and updating

--- roundsphere_django/roundsphere/mysite/home/models.py
+++ roundsphere_django/roundsphere/mysite/home/models.py
@@ -3,25 +3,37 @@ from django.urls import reverse
 
 import uuid # Required for unique IDs
 
-# Create your models here.
-#--------------------------------------------------------------------------------------------------------------------
-# User Model
+""" Create your models here."""
+"""--------------------------------------------------------------------------------------------------------------"""
+""" User Model """
 class User(models.Model):
+    ROLES = [
+        ('customer', 'Customer'),
+        ('admin', 'Admin'),
+        ('researcher', 'Researcher'),
+    ]
+      
     """Definition of the User Model in Django"""
-    profileId = models.AutoField(primary_key=True)
+    userId = models.AutoField(primary_key=True)
     # models.UUIDField(primary_key=True, default=uuid.uuid4)  # Auto-incrementing primary key
+
     firstname = models.CharField(max_length=80, null=False)  # First name, max length 80
     lastname = models.CharField(max_length=80, null=False)   # Last name, max length 80
-    email = models.EmailField(max_length=80, unique=True, null=False)  # Unique email field
-    role = models.CharField(max_length=80, null=False)       # Role field
+    email = models.EmailField(unique=True)  # Unique email address
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    postcode = models.CharField(max_length=10, blank=True, null=True)  # New field for postcode
+    role = models.CharField(max_length=20, choices=ROLES, default='researcher') # Role field
+    username = models.CharField(max_length=150, unique=True)  # Unique username
+    password = models.CharField(max_length=128)  # Hashed password
     created_at = models.DateTimeField(auto_now_add=True)     # Automatically set on creation
 
     def __str__(self):
-        return f"User {self.profileId}: {self.firstname} {self.lastname}"
+        return f"User {self.userId}: {self.firstname} {self.lastname}"
     
     def get_absolute_url(self):
         """Returns the URL to access a detail record for this user."""
-        return reverse('user-detail', args=[str(self.profileId)])
+        return reverse('user-detail', args=[str(self.userId)])
     
     class Meta:
         db_table = 'User_data'  # Explicitly sets the table name
@@ -29,57 +41,8 @@ class User(models.Model):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
-#--------------------------------------------------------------------------------------------------------------------
-# IoT Data Model
-class Iotdata(models.Model):
-    """Definition of the Iotdata Model in Django"""
-    dataId = models.AutoField(primary_key=True)  # Auto-incrementing primary key
-    temperatureWater = models.CharField(max_length=80, null=False)  # Water temperature as a string
-    temperatureAir = models.CharField(max_length=80, null=False)   # Air temperature as a string
-    humidity = models.CharField(max_length=80, null=False)         # Humidity as a string
-    windSpeed = models.CharField(max_length=80, null=False)        # Wind speed as a string
-    precipitation = models.CharField(max_length=80, null=False)    # Precipitation as a string
-    notes = models.TextField(null=False)                           # Notes with up to 200 characters
-    created_at = models.DateTimeField(auto_now_add=True)           # Automatically set on creation
-
-    def __str__(self):
-        return f"Iotdata {self.dataId}"
-    
-    def get_absolute_url(self):
-        """Returns the URL to access a detail record for this Apidata."""
-        return reverse('iotdata-detail', args=[str(self.dataId)])
-    
-    class Meta:
-        db_table = 'IoT_data'  # Explicitly sets the table name
-        ordering = ['-created_at']  # Orders by most recent data first
-        verbose_name = 'IoT Data'
-        verbose_name_plural = 'IoT Data Records'
-
-#--------------------------------------------------------------------------------------------------------------------
-# Analytic Data Model
-class Analytic(models.Model):
-    """Definition of the Analytic Model in Django"""
-    analyticId = models.AutoField(primary_key=True)  # Auto-incrementing primary key
-    region = models.CharField(max_length=80, null=False)  # Region field
-    evaporationRate = models.FloatField(null=True)  # Evaporation rate as a float
-    trend = models.CharField(max_length=80, null=False)  # Trend field
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for record creation
-
-    def __str__(self):
-        return f"Analytic {self.analyticId}: {self.region}"
-
-    def get_absolute_url(self):
-        """Returns the URL to access a detail record for this Analytic."""
-        return reverse('analytic-detail', args=[str(self.analyticId)])
-    
-    class Meta:
-        db_table = 'Analytic_data'  # Explicitly sets the table name
-        ordering = ['region', '-created_at']  # Orders by region, then most recent data
-        verbose_name = 'Analytic Data'
-        verbose_name_plural = 'Analytic Data Records'
-
-#--------------------------------------------------------------------------------------------------------------------
-# Product Model
+"""--------------------------------------------------------------------------------------------------------------"""
+""" Product Model """
 class Product(models.Model):
     """Definition of the Product Model in Django"""
     productId = models.AutoField(primary_key=True)  # Auto-incrementing primary key
@@ -101,6 +64,44 @@ class Product(models.Model):
         ordering = ['name']  # Orders alphabetically by product name
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
+        
+"""--------------------------------------------------------------------------------------------------------------"""
+""" Order Model """
+class Order(models.Model):
+    STATUS_CHOICES = [
+     ('Pending', 'Pending'),
+     ('Shipped', 'Shipped'),
+     ('Completed', 'Completed'),
+     ('Cancelled', 'Cancelled'),
+    ]
+    
+    """Definition of the Order Model in Django"""
+    orderId = models.AutoField(primary_key=True)  # Auto-incrementing primary key
+    userId = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to Django's User model
+    productId = models.ForeignKey(Product, on_delete=models.CASCADE)  # Link to Product model
+    quatity = models.PositiveIntegerField(null=True)  # Stock quantity as an integer
+    totalPrice = models.DecimalField(max_digits=10, decimal_places=2)  # Product price as a floating-point value
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp for when the product is created
+
+    def save(self, *args, **kwargs):
+        # Automatically calculate the total_price based on quantity and product price
+        if not self.totalPrice:
+            self.totalPrice = self.productId.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Order {self.orderId}: {self.userId.username}"
+    
+    def get_absolute_url(self):
+        """Returns the URL to access a detail record for this order."""
+        return reverse('order-detail', args=[str(self.orderId)])
+    
+    class Meta:
+        db_table = 'Order_data'  # Explicitly sets the table name
+        ordering = ['orderId']  # Orders alphabetically by Order Id
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
         
         
   
